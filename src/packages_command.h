aee6dcdab29f9dad9776f0543fdb7ae5bafbd879
mmorey25 <106971791+mmorey25@users.noreply.github.com>
2023-10-23 15:36:43 -0400

    Add files via upload

---
+++ src/packages_command.h
@@ -0,0 +1,48 @@
+#undef PACKAGE
+#define PACKAGE "KIM"
+#include "KIM/kim_command.h"
+#undef PACKAGE
+#define PACKAGE "MESSAGE"
+#include "MESSAGE/message.h"
+#undef PACKAGE
+#define PACKAGE "MESSAGE"
+#include "MESSAGE/server.h"
+#undef PACKAGE
+#define PACKAGE "PLUGIN"
+#include "PLUGIN/plugin.h"
+#undef PACKAGE
+#define PACKAGE "REPLICA"
+#include "REPLICA/hyper.h"
+#undef PACKAGE
+#define PACKAGE "REPLICA"
+#include "REPLICA/neb.h"
+#undef PACKAGE
+#define PACKAGE "REPLICA"
+#include "REPLICA/prd.h"
+#undef PACKAGE
+#define PACKAGE "REPLICA"
+#include "REPLICA/tad.h"
+#undef PACKAGE
+#define PACKAGE "REPLICA"
+#include "REPLICA/temper.h"
+#undef PACKAGE
+#define PACKAGE "SPIN"
+#include "SPIN/neb_spin.h"
+#undef PACKAGE
+#define PACKAGE "USER-COLVARS"
+#include "USER-COLVARS/group_ndx.h"
+#undef PACKAGE
+#define PACKAGE "USER-COLVARS"
+#include "USER-COLVARS/ndx_group.h"
+#undef PACKAGE
+#define PACKAGE "USER-MISC"
+#include "USER-MISC/temper_grem.h"
+#undef PACKAGE
+#define PACKAGE "USER-MISC"
+#include "USER-MISC/temper_npt.h"
+#undef PACKAGE
+#define PACKAGE "USER-PHONON"
+#include "USER-PHONON/dynamical_matrix.h"
+#undef PACKAGE
+#define PACKAGE "USER-PHONON"
+#include "USER-PHONON/third_order.h"
