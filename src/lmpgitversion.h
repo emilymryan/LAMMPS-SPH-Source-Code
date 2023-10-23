a3249ebabd85dfb1e798f68ea1c31b7a0e2b170a
mmorey25 <106971791+mmorey25@users.noreply.github.com>
2023-10-23 15:30:12 -0400

    Add files via upload

---
+++ src/lmpgitversion.h
@@ -0,0 +1,7 @@
+#ifndef LMP_GIT_VERSION_H
+#define LMP_GIT_VERSION_H
+const bool LAMMPS_NS::LAMMPS::has_git_info = true;
+const char LAMMPS_NS::LAMMPS::git_commit[] = "f6f7ab98e93cd0a2409f3fab61f415c54e2af728";
+const char LAMMPS_NS::LAMMPS::git_branch[] = "bv-reaction";
+const char LAMMPS_NS::LAMMPS::git_descriptor[] = "patch_8Apr2021-304-gf6f7ab9-modified";
+#endif
