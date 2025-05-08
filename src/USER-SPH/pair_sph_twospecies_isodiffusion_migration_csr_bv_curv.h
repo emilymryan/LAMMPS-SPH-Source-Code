6610af819f4c3124f96624d2e3371cd5fa03b2a3
mmorey25 <106971791+mmorey25@users.noreply.github.com>
2025-05-08 11:05:14 -0400

    Add files via upload

---
+++ src/USER-SPH/pair_sph_twospecies_isodiffusion_migration_csr_bv_curv.h
@@ -0,0 +1,44 @@
+#ifdef PAIR_CLASS
+
+PairStyle(sph/twospecies/isodiffusion/migration/csr/bv/curv, PairSPHTwospeciesIsodiffusionMigrationCSRbvcurv)
+
+#else
+
+#ifndef LMP_PAIR_SPH_TWOSPECIES_ISODIFFUSION_MIGRATION_CSR_BV_CURV_H
+#define LMP_PAIR_SPH_TWOSPECIES_ISODIFFUSION_MIGRATION_CSR_BV_CURV_H
+
+#include "pair.h"
+
+namespace LAMMPS_NS {
+  class PairSPHTwospeciesIsodiffusionMigrationCSRbvcurv : public Pair {
+  public:
+    PairSPHTwospeciesIsodiffusionMigrationCSRbvcurv(class LAMMPS *);
+    virtual ~PairSPHTwospeciesIsodiffusionMigrationCSRbvcurv();
+    void init_style();
+    virtual void compute(int, int);
+    void settings(int, char **);
+    void coeff(int, char **);
+    virtual double init_one(int, int);
+    virtual double single(int, int, int, int, double, double, double, double &);
+
+    int pack_forward_comm(int, int *, double *, int, int *);
+    void unpack_forward_comm(int, int, double *);
+
+    int pack_reverse_comm(int, int, double *);
+    void unpack_reverse_comm(int, int *, double *);
+
+  protected:
+    // cCeq is the equilibrium concentration for the cations at the anode
+    double applied_pot, cCeq, cA_init, cC_init, mass_init;
+    // cut represents the size of kernel support
+    double **cut;
+    // phase_support is the size of the inter-phase interaction
+    double **phase_support;
+    double *cA, *dcA ,*cC, *dcC,*mM, *dmM, *local_pot;
+    double *muA, *muC, *DA, *DC, *RC, *nx, *ny, *nz;
+    void allocate();
+  };
+}
+
+#endif
+#endif
