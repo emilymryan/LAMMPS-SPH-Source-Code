0b350f892bc743b43f5180f4cfe1ff3b2406d121
mmorey25 <106971791+mmorey25@users.noreply.github.com>
2025-05-08 11:01:16 -0400

    Add files via upload

---
+++ src/USER-SPH/fix_sph_concentration_mass_solv.cpp
@@ -0,0 +1,183 @@
+/* ----------------------------------------------------------------------
+ LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+ http://lammps.sandia.gov, Sandia National Laboratories
+ Steve Plimpton, sjplimp@sandia.gov
+
+ Copyright (2003) Sandia Corporation.  Under the terms of Contract
+ DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+ certain rights in this software.  This software is distributed under
+ the GNU General Public License.
+
+ See the README file in the top-level LAMMPS directory.
+ ------------------------------------------------------------------------- */
+
+#include <stdio.h>
+#include <string.h>
+#include "fix_sph_concentration_mass_solv.h"
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include "atom.h"
+#include "comm.h"
+#include "force.h"
+#include "neighbor.h"
+#include "neigh_list.h"
+#include "neigh_request.h"
+#include "update.h"
+#include "integrate.h"
+#include "respa.h"
+#include "memory.h"
+#include "error.h"
+#include "pair.h"
+#include <cstdio>
+
+using namespace LAMMPS_NS;
+using namespace FixConst;
+
+/* ---------------------------------------------------------------------- */
+
+FixSPHConcentrationMassSolv::FixSPHConcentrationMassSolv(LAMMPS *lmp, int narg, char **arg) :
+  Fix(lmp, narg, arg) {
+  if ((atom->esph_flag != 1) || (atom->rho_flag != 1))
+    error->all(FLERR,
+	       "fix sph/concentration/mass/solv command requires atom_style with both energy and density, e.g. meso");
+
+  if (narg != 3)
+    error->all(FLERR,"Illegal number of arguments for fix sph/concentration/mass/solv command");
+
+  time_integrate = 0;
+
+  // find the concentration property
+  int fcA;
+  int icA = atom->find_custom("cA", fcA);
+  if (icA < 0)
+    error->all(FLERR,
+	       "Can't find property cA for fix sph/concentration/mass/solv");
+  cA = atom->dvector[icA];
+
+  // find the concentration property
+  int fdcA;
+  int idcA = atom->find_custom("dcA", fdcA);
+  if (icA < 0)
+    error->all(FLERR,
+	       "Can't find property dcA for fix sph/concentration/mass/solv");
+  dcA = atom->dvector[idcA];
+
+  // find the concentration property
+  int fcC;
+  int icC = atom->find_custom("cC", fcC);
+  if (icC < 0)
+    error->all(FLERR,
+	       "Can't find property cC for fix sph/concentration/mass/solv");
+  cC = atom->dvector[icC];
+
+  // find the concentration property
+  int fdcC;
+  int idcC = atom->find_custom("dcC", fdcC);
+  if (icC < 0)
+    error->all(FLERR,
+	       "Can't find property dcC for fix sph/concentration/mass/solv");
+  dcC = atom->dvector[idcC];
+
+  // find the concentration property                                                                                                                                        
+  int fcS;
+  int icS = atom->find_custom("cS", fcS);
+  if (icS < 0)
+    error->all(FLERR,
+               "Can't find property cS for fix sph/concentration/mass/solv");
+  cS = atom->dvector[icS];
+
+  // find the concentration property                                                                                                                                        
+  int fdcS;
+  int idcS = atom->find_custom("dcS", fdcS);
+  if (icS < 0)
+    error->all(FLERR,
+               "Can't find property dcS for fix sph/concentration/mass/solv");
+  dcS = atom->dvector[idcS];
+  
+// find the concentration property
+  int fhH;
+  int ihH = atom->find_custom("hH", fhH);
+  if (ihH < 0)
+    error->all(FLERR,
+               "Can't find property hH for fix sph/concentration/mass/solv");
+  hH = atom->dvector[ihH];
+
+  // find the concentration property                                                                                           
+  int fdhH;
+  int idhH = atom->find_custom("dhH", fdhH);
+  if (ihH < 0)
+    error->all(FLERR,
+	       "Can't find property dhH for fix sph/concentration/mass/solv");
+  dhH = atom->dvector[idhH];
+  
+  // find the mass of A property
+  int fmM;
+  int imM = atom->find_custom("mM", fmM);
+  if (imM < 0)
+    error->all(FLERR,
+	       "Can't find property mM for fix sph/concentration/mass");
+  mM = atom->dvector[imM];
+
+  // find the solid-liquid interaction
+  int fdmM;
+  int idmM = atom->find_custom("dmM", fdmM);
+  if (idmM < 0)
+    error->all(FLERR,
+	       "Can't find property dmM for fix sph/concentration/mass");
+  dmM = atom->dvector[idmM];
+
+
+
+}
+
+/* ---------------------------------------------------------------------- */
+
+FixSPHConcentrationMassSolv::~FixSPHConcentrationMassSolv() {
+}
+
+/* ---------------------------------------------------------------------- */
+
+int FixSPHConcentrationMassSolv::setmask() {
+  int mask = 0;
+  mask |= FINAL_INTEGRATE;
+  return mask;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixSPHConcentrationMassSolv::init() {
+  dtcA = update->dt;
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixSPHConcentrationMassSolv::final_integrate() {
+  int *mask = atom->mask;
+  int nlocal = atom->nlocal;
+  int *type = atom->type;
+
+  double **x = atom->x;
+
+  if (igroup == atom->firstgroup)
+    nlocal = atom->nfirst;
+ 
+  for (int i = 0; i < nlocal; i++) {
+    if (mask[i] & groupbit) {
+      cA[i] += dtcA*dcA[i];
+      cC[i] += dtcA*dcC[i];
+      cS[i] += dtcA*dcS[i];
+      hH[i] += dtcA*dhH[i];
+      // Only update mass for solid particles
+      if (type[i] == 2){
+	mM[i] += dtcA*dmM[i];
+      }
+    }
+  }
+}
+
+/* ---------------------------------------------------------------------- */
+
+void FixSPHConcentrationMassSolv::reset_dt() {
+  dtcA = update->dt;
+}
